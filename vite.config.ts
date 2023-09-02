9137e89de90730072aa822f775fa260db3d83d86
酷甚么喵 <btguigushi@qq.com>
2023-09-03 00:43:45 +0800

    feat: 优化cdn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { Plugin as importToCDN, autoComplete } from 'vite-plugin-cdn-import'
+import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,22 +8,26 @@ export default defineConfig({
     vue(),
     importToCDN({
       modules: [
-        autoComplete('vue'),
+        {
+          name: 'vue',
+          var: 'Vue',
+          path: 'https://cdn.staticfile.org/vue/3.3.4/vue.global.prod.min.js',
+        },
         {
           name: '@wangeditor/editor',
           var: 'wangEditor',
-          path: 'https://unpkg.com/@wangeditor/editor@latest/dist/index.js',
+          path: 'https://cdn.staticfile.org/wangeditor5/5.1.23/index.min.js',
         },
         {
           name: 'element-plus',
           var: 'ElementPlus',
-          path: 'https://cdn.jsdelivr.net/npm/element-plus',
-          css: 'https://cdn.jsdelivr.net/npm/element-plus/dist/index.css',
+          path: 'https://cdn.staticfile.org/element-plus/2.3.12/index.full.min.js',
+          css: 'https://cdn.staticfile.org/element-plus/2.3.12/index.min.css',
         },
         {
           name: '@element-plus/icons-vue',
           var: 'ElementPlusIconsVue',
-          path: 'https://cdn.jsdelivr.net/npm/@element-plus/icons-vue',
+          path: 'https://cdn.staticfile.org/element-plus-icons-vue/2.1.0/global.iife.min.js',
         },
       ],
     }),
