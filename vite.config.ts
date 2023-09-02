4954ba4e4aa24b941b82aa98c6372ec7260099f6
酷甚么喵 <btguigushi@qq.com>
2023-09-02 20:34:43 +0800

    feat: 使用CDN减少打包体积

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ElementPlus from 'unplugin-element-plus/vite'
+import { Plugin as importToCDN, autoComplete } from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ElementPlus({})],
+  plugins: [
+    vue(),
+    importToCDN({
+      modules: [
+        autoComplete('vue'),
+        {
+          name: '@wangeditor/editor',
+          var: 'wangEditor',
+          path: 'https://unpkg.com/@wangeditor/editor@latest/dist/index.js',
+        },
+        {
+          name: 'element-plus',
+          var: 'ElementPlus',
+          path: 'https://cdn.jsdelivr.net/npm/element-plus',
+          css: 'https://cdn.jsdelivr.net/npm/element-plus/dist/index.css',
+        },
+        {
+          name: '@element-plus/icons-vue',
+          var: 'ElementPlusIconsVue',
+          path: 'https://cdn.jsdelivr.net/npm/@element-plus/icons-vue',
+        },
+      ],
+    }),
+  ],
   server: {
     port: 8077,
   },
