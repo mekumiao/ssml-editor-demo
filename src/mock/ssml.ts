8d0995a8afa4b6180bcc9b97491a2f7bb6a7c7c4
mekumiao <btguigushi@qq.com>
2023-11-05 22:54:40 +0800

    feat: update package

--- src/mock/ssml.ts
+++ src/mock/ssml.ts
@@ -1,23 +1,23 @@
-export const html = `<p>你好啊!<span
+export const html = `<paragraph data-ow-remark="mekumiao/ssml-editor">你好啊! <span
+data-w-e-type="ssml-sub"
+data-w-e-is-inline
+data-ow-remark="兄台"
+data-ow-alias="兄台"
+>兄嘚</span> <span
 data-w-e-type="ssml-break"
 data-w-e-is-void
 data-w-e-is-inline
-data-ow-remark="中"
+data-ow-remark="短"
 data-ow-time=""
-data-ow-strength="medium"
-></span> &nbsp;<span
-data-w-e-type="ssml-sub"
-data-w-e-is-inline
-data-ow-remark="兄嘚"
-data-ow-alias="兄嘚"
->兄台</span> </p><p><span
+data-ow-strength="weak"
+></span></paragraph><paragraph data-ow-remark="mekumiao/ssml-editor"><span
 data-w-e-type="custom-management"
 data-w-e-is-inline
-data-ow-remark="晓萱|年长成年男性|冷静|1.0|0"
-data-ow-style="calm"
-data-ow-name="zh-CN-XiaoxuanNeural"
+data-ow-remark="云泽|年长成年男性|快乐|1.0|0"
+data-ow-style="cheerful"
+data-ow-name="zh-CN-YunzeNeural"
 data-ow-role="OlderAdultMale"
 data-ow-pitch="0"
 data-ow-rate="1.0"
-data-ow-custom='{"contentData":{"category":"","name":"zh-CN-XiaoxuanNeural","role":"OlderAdultMale","style":"calm","speed":"1.0","pitch":"0"}}'
->今天天气这么样?</span> </p>`
+data-ow-custom='{"contentData":{"category":"","name":"zh-CN-YunzeNeural","role":"OlderAdultMale","style":"cheerful","speed":"1.0","pitch":"0"}}'
+>今天天气怎么样?</span> </paragraph>`
