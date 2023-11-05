56d4d2973eb0d4e957d2e8cb96ab9922055e006c
mekumiao <btguigushi@qq.com>
2023-11-06 00:55:13 +0800

    fix: bug修复

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import type { IDomEditor } from '@wangeditor/editor'
 import { ElButton, ElMessage } from 'element-plus'
 import dayjs from 'dayjs'
 
+const siteDomain = 'https://ssml.sdaxia.top'
 let siteHtml: string = ''
 
 function handleCreated(editor: IDomEditor) {
@@ -28,21 +29,26 @@ async function handleExportHtml() {
 
 async function renderHtml(html: string) {
   if (!siteHtml) {
-    const response = await fetch('https://ssml.sdaxia.top')
+    const response = await fetch(siteDomain)
     if (response.status !== 200) {
       throw new Error(`获取网页源代码失败，状态码：${response.status}`)
     }
     siteHtml = await response.text()
   }
   const scriptElement = document.createElement('script')
-  scriptElement.type = 'module'
   scriptElement.textContent = `window.html = \`${html}\``
 
   const parser = new DOMParser()
   const doc = parser.parseFromString(siteHtml, 'text/html')
 
+  const scriptElem = doc.querySelector('script[src^="/assets/index-"]')
+  scriptElem?.setAttribute('src', `${siteDomain}${scriptElem.getAttribute('src')}`)
+
+  const linkElem = doc.querySelector('link[href^="/assets/index-"]')
+  linkElem?.setAttribute('href', `${siteDomain}${linkElem.getAttribute('href')}`)
+
   const headElement = doc.querySelector('head')
-  headElement?.appendChild(scriptElement)
+  scriptElem && headElement?.insertBefore(scriptElement, scriptElem)
 
   return doc.documentElement.innerHTML
 }
